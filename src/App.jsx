47b8258a77a5c7994fe78e0c0b95f1ce6fa8f99a
Yuvraj Sirohi <yuvrajsirohi22@gmail.com>
2023-03-31 01:52:35 -0700

    add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.scss';
-import {BrowserRouter, Routes, Route } from "react-dom"
+import {BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <BrowserRouter >
     <Header />
     <Routes >
-    <Route path="/" element={<HomePage />}></Route> 
+    {/* <Route path="/" element={<HomePage />}></Route>  */}
     </Routes>
 
    </BrowserRouter>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,54 @@
+import "./Header.scss";
+import {NavLink} from "react-router-dom";
+import Logo from "../../assets/logo/Logo.svg"
+import home from "../../assets/icons/1-home.svg"
+import search from "../../assets/icons/2-search.svg"
+import open from "../../assets/icons/3-open-book.svg"
+import text from "../../assets/icons/4-text-align-left-bold.svg"
+import list from "../../assets/icons/5-list-bullets-fill.svg"
+import dots from "../../assets/icons/6-dots-square.svg"
+import compass from "../../assets/icons/7-compass.svg"
+import arrow from "../../assets/icons/8-arrow-square.svg"
+import shooting from "../../assets/icons/9-shooting-star-bold.svg"
+import question from "../../assets/icons/10-question-circle-fill.svg"
+import round from "../../assets/icons/11-round-account-circle.svg"
+
+
+
+
+
+function Header() {
+    return (
+        <header className="header">
+            <nav className="nav">
+            <div className="nav-top">
+            <img className='nav__logo' src={Logo} alt="Visier Logo"/>
+
+            <NavLink to='/'>
+            {/* <NavLink to="/"
+                    className={({ isActive }) =>
+                    isActive ? "nav__link nav__link--active" : "nav__link"
+                    }> */}
+                <img className="nav__icon" src={home} alt="Home logo"/>
+            </NavLink>
+            <img className="nav__icon" src={search} alt="Search logo"/>
+            <img className="nav__icon" src={open} alt="Open book logo"/>
+            <img className="nav__icon" src={text} alt="Text align left logo"/>
+            <img className="nav__icon" src={list} alt="List bullet logo"/>
+            <img className="nav__icon" src={dots} alt="Square dot logo"/>
+            <img className="nav__icon" src={compass} alt="Compass logo"/>
+            <img className="nav__icon" src={arrow} alt="Arrow Square logo"/>
+
+            </div>
+            <div className="nav-bottom">
+              <img className="nav__icon" src={shooting} alt="Star logo"/>
+              <img className="nav__icon" src={question} alt="Question logo"/>
+              <img className="nav__icon" src={round} alt="Round logo"/>
+            </div>
+            </nav>
+        </header>
+    )
+}
+
+
+export default Header;
